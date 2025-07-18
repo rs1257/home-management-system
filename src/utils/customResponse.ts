1e8c1aa9202837924c7f1b1f8e3c56330b39b66f
Ryan Smith <ryansmith300082@gmail.com>
2025-07-18 19:09:15 +0100

    feat: additional cors fixes

--- src/utils/customResponse.ts
+++ src/utils/customResponse.ts
@@ -6,6 +6,10 @@ export class CustomResponse extends Response {
     super(body, init);
     this.headers.set('Access-Control-Allow-Origin', '*');
     this.headers.set('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    this.headers.set(
+      'Access-Control-Allow-Headers',
+      'access-control-allow-origin, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+    );
 
     if (String(init?.status ?? 200).startsWith('2')) {
       logger.info(`Response created with status: ${init?.status ?? 200}, body: ${body ? JSON.stringify(body) : 'null'}`);
